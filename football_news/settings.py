c2ddcf4f445b5fb4fe57189a6c92fb430e7070ae
Slamooyn <leparhoy@gmail.com>
2025-09-10 16:15:05 +0700

    test

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","muhammad-salman42-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://muhammad-salman42-footballnews.pbp.cs.ui.ac.id/"
+    "https://muhammad-salman42-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
