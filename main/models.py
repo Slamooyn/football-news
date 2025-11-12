a7e0399af3f0f7db1132699f5f3e82966eddfad0
Slamooyn <leparhoy@gmail.com>
2025-11-12 13:38:33 +0700

    test

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     content = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
